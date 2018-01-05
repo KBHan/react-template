89f7d7fd731fe032a25d02bbfa03e2a7f25a36aa
KBHan <kbhanlive@gmail.com>
2018-01-05 01:10:07 -0700

    Contact feature added

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
+import Contact from './Contact';
 
 class App extends React.Component {
-  constructor (props){
-    super(props);
-    this.state = {name:''};
-  }
     render(){
         return (
-          <div>
-            <button onClick={()=>{this.setState({name:'World'});}}>click</button>
-            <h1>Hello {this.state.name}</h1>
-          </div>
+          <Contact/>
         );
     }
 }
